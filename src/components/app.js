56062894e33ba23b3298f975ab2c4acb45829a55
Mac <mac@MacBook-Pro.local>
2018-05-13 17:30:11 -0600

    added .gitignore file, and changed title

--- src/components/app.js
+++ src/components/app.js
@@ -4,7 +4,7 @@ export default class App extends Component {
   render() {
     return (
       <div>
-        <h1>DevCamp React Starter</h1>
+        <h1>Osmosisss Productions Agreement</h1>
       </div>
     );
   }
